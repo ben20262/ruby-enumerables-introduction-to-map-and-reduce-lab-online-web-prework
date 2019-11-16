cea0702570dc1fb0573dedec38e36d5d56500ff2
Benjamin Small <ben20262@aim.com>
2019-11-16 01:11:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
-# My Code here....
+def map_to_negativize
+end
+
+def map_to_no_change
+end
+
+def map_to_double
+end
+
+def map_to_square
+end
+
+
+def reduce_to_total
+end
+
+def reduce_to_all_true
+end
+
+def reduce_to_any_true
+end
